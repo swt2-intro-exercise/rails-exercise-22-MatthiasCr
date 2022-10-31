6569b102af4e72bf47958ea77a2c877d9b37eaad
MatthiasCr <matthias.cram@gmail.com>
2022-10-31 13:50:20 +0100

    test for #4

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_27_100421) do
     t.string "first_name"
     t.string "last_name"
     t.string "homepage"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,11 +5,26 @@ require 'rails_helper'
      # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
      visit new_author_path
    end
+
    it "should have text input fields for an author's first name, last name and homepage" do
      visit new_author_path
-     # these are the standard names given to inputs by the Rails form builder
+
      expect(page).to have_field('author[first_name]')
      expect(page).to have_field('author[last_name]')
      expect(page).to have_field('author[homepage]')
+   end
+
+   it "should save a new author in the database on submit" do
+    # Given
+    last_name = 'Turing'
+    author = Author.new(:last_name => last_name)
+
+    # When
+    visit new_author_path
+    page.fill_in('author[last_name]', with: last_name)
+    find('input[type="submit"]').click
+
+    # Then
+    expect(Author.last).to eq(author)
    end
  end

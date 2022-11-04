22c6bf6af7b1f897d1ed09d600bd8bbad7361ee8
MatthiasCr <matthias.cram@gmail.com>
2022-11-04 16:02:52 +0100

    paper page shows authors - resolves #15

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -5,4 +5,17 @@ require 'rails_helper'
       @cmi = FactoryBot.create :paper
       visit paper_path(@cmi)
     end
+
+    it "should display the authors full names" do
+      # Given
+      @paper = FactoryBot.create :paper
+      @author = FactoryBot.create :author
+      @paper.authors << @author
+
+      # When
+      visit paper_path(@paper)
+
+      # Then
+      expect(page).to have_text(@author.name)
+    end
  end

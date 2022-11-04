4a9f497f201aa9a78b6c00bc9c32bfac11e53546
MatthiasCr <matthias.cram@gmail.com>
2022-11-04 14:14:04 +0100

    paper should not validate without attributes - resolves #12

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,11 @@
 class Paper < ApplicationRecord
+  validates :title, :venue, :year, presence: true
+  validates :year, numericality: true
+
+  def ==(o)
+    o.class == self.class &&
+    o.title == self.title &&
+    o.venue == self.venue &&
+    o.year == self.year
+  end
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
-RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+ describe "Paper", type: :model do
+  it "should not validate without title" do
+    # Given
+    venue = "Mind 49: 433-460"
+    year = 1950
+    paper = Paper.new(:venue => venue, :year => year)
+
+    # Then
+    expect(paper).to_not be_valid
+  end
+
+  it "should not validate without venue" do
+    # Given
+    title = "COMPUTING MACHINERY AND INTELLIGENCE"
+    year = 1950
+    paper = Paper.new(:title => title, :year => year)
+
+    # Then
+    expect(paper).to_not be_valid
+  end
+
+  it "should not validate without year" do
+    # Given
+    title = "COMPUTING MACHINERY AND INTELLIGENCE"
+    venue = "Mind 49: 433-460"
+    paper = Paper.new(:title => title, :venue => venue)
+
+    # Then
+    expect(paper).to_not be_valid
+  end
+
+  it "should not validate with a not numeric year value" do
+    # Given
+    title = "COMPUTING MACHINERY AND INTELLIGENCE"
+    venue = "Mind 49: 433-460"
+    year  = "lololol"
+    paper = Paper.new(:title => title, :venue => venue, :year => year)
+
+    # Then
+    expect(paper).to_not be_valid
+  end
+
+ end
